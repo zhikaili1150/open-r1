3f566740a6c05d91c78b53d125300ca4eb547d6a
Zhikai <luckydogqaq@163.com>
2025-09-08 04:05:18 -0400

    fix: prevent overwriting eval_times.csv by only writing header if file doesn't exist

--- eval.sh
+++ eval.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+#SBATCH --job-name=exp_lr_eval
+#SBATCH --output=logs/%j_exp_lr_eval.out
+#SBATCH --error=logs/%j_exp_lr_eval.err
+#SBATCH --partition=h100
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32GB
+
 
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
@@ -9,8 +19,8 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 # uv
 source openr1/bin/activate
 
-BASE_MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
-EXP_NAME="exp14_dsqwen7b_openrs"
+BASE_MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
+EXP_NAME="exp_lr_1e-07"
 
 # =========================
 # Mode selection
@@ -20,49 +30,70 @@ LORA_DIR=""   # e.g., "data/OpenRS-GRPO/210"
 
 # Mode 2: manually specify LoRA checkpoint list
 LORA_LIST=(
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-40"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-80"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-120"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-160"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-200"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-240"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-280"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-320"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-360"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-400"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-22"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-44"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-66"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-88"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-110"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-132"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-154"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-176"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-198"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-220"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-242"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-264"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-286"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-308"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-330"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-352"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-374"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-396"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110/checkpoint-418"
+    "/local/scratch/zli2255/workspace/open-r1/experiments/exp_lr/ckpt/110"
 
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-40"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-80"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-120"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-160"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-200"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-240"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-280"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-320"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-360"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-400"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-40"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-80"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-120"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-160"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-200"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-240"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-280"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-320"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-360"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/mix_reward/211/checkpoint-400"
 
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-40"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-80"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-120"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-160"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-200"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-240"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-280"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-320"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-360"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-400"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-40"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-80"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-120"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-160"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-200"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-240"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-280"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-320"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-360"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/merge_policy/211-110_101_1_1/checkpoint-400"
 
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-40"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-80"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-120"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-160"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-200"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-240"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-280"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-320"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-360"
-    "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-400"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-40"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-80"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-120"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-160"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-200"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-240"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-280"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-320"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-360"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/101/checkpoint-400"
+
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-40"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-80"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-120"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-160"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-200"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-240"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-280"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-320"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-360"
+    # "experiments/exp14_dsqwen7b_openrs/ckpt/dsqwen7b_openrs/110/checkpoint-400"
 )
 
 
@@ -80,13 +111,17 @@ fi
 
 # TASKS=("aime24" "math_500" "amc23" "minerva" "olympiadbench")
 # TASKS=("aime24" "math_500" "amc23" "minerva" "olympiadbench")
-TASKS=("aime24" "amc23")
+# TASKS=("aime24" "amc23" "math_500")
+TASKS=("math_500")
 
 # CSV file
 RESULTS_DIR="results/$EXP_NAME"
 mkdir -p "$RESULTS_DIR"
 TIMING_CSV_FILE="$RESULTS_DIR/eval_times.csv"
-echo "model,task,start_time,end_time,duration" > "$TIMING_CSV_FILE"
+
+if [ ! -f "$TIMING_CSV_FILE" ]; then
+    echo "model,task,start_time,end_time,duration" > "$TIMING_CSV_FILE"
+fi
 
 for LORA_PATH in "${LORA_PATHS[@]}"; do
     echo "=========================================="
